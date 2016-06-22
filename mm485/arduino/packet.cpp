d29bd82f67966452c1566c70b959ca1089849ce6
blackdog70 <sbonisolo@gmail.com>
2016-06-22 21:47:46 +0200

    Fixed crc calculation

--- mm485/arduino/packet.cpp
+++ mm485/arduino/packet.cpp
@@ -8,7 +8,6 @@
 #include "packet.h"
 #include "string.h"
 #include "stdio.h"
-//#include "codec128.h"
 
 Packet::Packet() {
 	source = 0;
@@ -30,7 +29,7 @@ Packet::Packet(uint8_t src, uint8_t dst, const unsigned char* d, size_t size) {
 	crc = crc_calculate();
 }
 
-Packet::Packet(uint8_t src, uint8_t dst, unsigned short id, const unsigned char* d, size_t size) {
+Packet::Packet(uint8_t src, uint8_t dst, uint16_t id, const unsigned char* d, size_t size) {
 	source = src;
 	dest = dst;
 	length = size;
@@ -39,30 +38,28 @@ Packet::Packet(uint8_t src, uint8_t dst, unsigned short id, const unsigned char*
 	crc = crc_calculate();
 }
 
-short Packet::id_calculate() {
+uint16_t Packet::id_calculate() {
 	return crc_calculate();
 }
 
-unsigned long Packet::crc_calculate() {
-	uint8_t s_len = 2+length;
+uint16_t Packet::crc_calculate() {
+	size_t s_len = 2+length;
 	char s[s_len];
 
 	s[0] = dest;
 	s[1] = length;
-	memcpy((char *)(s+2), data, length);
+	memcpy((uint8_t *)(s+2), data, length);
 
-//	Serial.print("crc=");
+//	Serial.println("==crc==");
 //	for(unsigned int i=0; i<s_len; i++) {
-//		Serial.print(s[i], 16);
+//		Serial.print(s[i] & 255, HEX);
 //		Serial.print(" ");
 //	}
+//	Serial.println();
+//	Serial.println(crc16(s, s_len), HEX);
+//	Serial.println("==crc==");
 
-	unsigned short crc_modbus = 0xffff;
-	const char *p = s;
-	for (;p < s + s_len; p++)
-		crc_modbus = update_crc_16(crc_modbus, *p);
-
-    return crc_modbus;
+    return crc16(s, s_len);
 }
 
 bool Packet::validate() {
@@ -75,9 +72,6 @@ Packet* Packet::deserialize(const unsigned char* msg) {
 	packet_id = ((unsigned char)msg[2] << 8) | (unsigned char)msg[3];
 	length = msg[4];
 	memcpy(data, (char *)(msg+5), length);
-//	char l = msg[4];
-//	length = dec128(data, (unsigned char*)(msg + 5), l);
-//	data[(int)length] = 0;
 	crc = ((unsigned char)msg[5+length] << 8) | (unsigned char)msg[6+length];
 	return this;
 }
@@ -85,15 +79,21 @@ Packet* Packet::deserialize(const unsigned char* msg) {
 size_t Packet::serialize(unsigned char *msg) {
 	msg[0] = source;
 	msg[1] = dest;
-	msg[2] = (unsigned char)(packet_id >> 8);
-	msg[3] = (unsigned char)(packet_id & 0xFFu);
+	msg[2] = (unsigned char)((packet_id >> 8) & 0xff);
+	msg[3] = (unsigned char)(packet_id & 0xff);
 	msg[4] = length;
 	memcpy((char *)(msg+5), data, length);
-//	char l = enc128((unsigned char*)(msg + 5), data, length);
-//	msg[4] = l;
-	msg[5+length] = (unsigned char)(crc >> 8);
-	msg[6+length] = (unsigned char)(crc & 0xFFu);
-//	msg[7+length] = EOM;
-//	return 8+length;
-	return 7+length;
+	msg[5+length] = (unsigned char)((crc >> 8) & 0xff);
+	msg[6+length] = (unsigned char)(crc & 0xff);
+	msg[7+length] = EOP;
+
+//	Serial.println("==serialize==");
+//	for(unsigned int i=0; i<7+length; i++) {
+//		Serial.print(msg[i] & 255, HEX);
+//		Serial.print(" ");
+//	}
+//	Serial.println();
+//	Serial.println("==serialize==");
+
+	return 8+length;
 }
--- mm485/arduino/packet.h
+++ mm485/arduino/packet.h
@@ -9,9 +9,9 @@
 #define PACKET_H_
 
 #include "Arduino.h"
-#include "Crc.h"
+#include <crc16.h>
 
-#define MAX_DATA_SIZE 50
+#define MAX_DATA_SIZE 16
 #define MAX_PACKET_SIZE 8+MAX_DATA_SIZE+1 // Take care do not exceed 254 chars 255=0xff if for EOM
 #define EOP (uint8_t)0xff
 #define ACK (uint8_t)0xfd
@@ -23,24 +23,25 @@ class Packet {
 public:
 	uint8_t source;
 	uint8_t dest;
-    unsigned short packet_id;
+	uint16_t packet_id;
     size_t length;
     unsigned char data[MAX_DATA_SIZE];
-    unsigned short crc;
+    uint16_t crc;
     uint8_t retry;
     unsigned long timeout;
 
     Packet();
     Packet(uint8_t source, uint8_t dest, const unsigned char* data, size_t size);
-    Packet(uint8_t source, uint8_t dest, unsigned short packet_id, const unsigned char* data, size_t size);
+    Packet(uint8_t source, uint8_t dest, uint16_t packet_id, const unsigned char* data, size_t size);
+//	virtual ~Packet() {};
 	virtual ~Packet() {};
 	bool validate();
 	Packet* deserialize(const unsigned char* data);
 	size_t serialize(unsigned char* data);
 
 //private:
-	short id_calculate();
-	unsigned long crc_calculate();
+	uint16_t id_calculate();
+	uint16_t crc_calculate();
 };
 
 #endif /* PACKET_H_ */
